ce57c2e647960215ec657bbc74b53e18fd651a21
Zachary Porter <zporter9@gmail.com>
2010-12-11 11:32:38 -0500

    cucumber steps defined for starting game

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -2,10 +2,12 @@ Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-  Codebreaker::Game.new.start
+  game = Codebreaker::Game.new(output)
+  game.start
 end
 
-Then /^I should see "([^"]*)"$/ do |arg1|
+Then /^I should see "([^"]*)"$/ do |message|
+  output.messages.should include(message)
 end
 
 Given /^the secret code is "([^"]*)"$/ do |arg1|
@@ -15,4 +17,18 @@ When /^I guess "([^"]*)"$/ do |arg1|
 end
 
 Then /^the mark should be "([^"]*)"$/ do |arg1|
+end
+
+class Output
+  def messages
+    @messages ||= []
+  end
+  
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||= Output.new
 end
--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,5 +1,9 @@
 module Codebreaker
   class Game
+    def initialize(output)
+      
+    end
+    
     def start
       
     end
