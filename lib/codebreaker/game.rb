17f128e0cfccaf8186dac5fa62d8c4f0425692c5
Zachary Porter <zporter9@gmail.com>
2011-01-16 16:13:24 -0500

    refactored by creating a marker class to handle the marking algorithm

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -10,27 +10,9 @@ module Codebreaker
       @output.puts 'Enter guess:'
     end
     
-    def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-      (0..3).each do |index|
-        if number_match?( guess, index )
-          mark << '-'
-        end
-      end
-      @output.puts mark
-    end
-    
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-    
-    def number_match?(guess, index)
-      @secret.include?( guess[index] ) && !exact_match?(guess, index)
+    def guess(guess)      
+      marker = Marker.new(@secret, guess)
+      @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
     end
   end
 end
