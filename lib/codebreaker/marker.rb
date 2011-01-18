6d715800de74a6bb5e7e9d8e2d09505050f664f9
Zachary Porter <zporter9@gmail.com>
2011-01-18 18:58:30 -0500

    refactored code to handle matches with duplicates

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -10,9 +10,18 @@ class Marker
   end
 
   def number_match_count
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
+    total_match_count - exact_match_count
+  end
+  
+  def total_match_count
+    secret = @secret.split('')
+    @guess.split('').inject(0) do |count, n|
+      count + (delete_first(secret, n) ? 1 : 0)
     end
+  end
+  
+  def delete_first(code, n)
+    code.delete_at(code.index(n)) if code.index(n)
   end
 
   def exact_match?(index)
